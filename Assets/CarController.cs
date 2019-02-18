15ad7adee49ed25aa8167ad279a84000657b1911
Minchang Cheon <43161094+ingmcc123@users.noreply.github.com>
2019-02-19 04:15:17 +0900

    Add files via upload

---
+++ Assets/CarController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    float speed = 0;
+    Vector2 startPos;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+
+        if(Input.GetMouseButtonDown(0)) {
+            this.startPos = Input.mousePosition;
+        } else if(Input.GetMouseButtonUp(0)) {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = (endPos.x - this.startPos.x);
+
+            this.speed = swipeLength / 500.0f;
+
+            GetComponent<AudioSource>().Play();
+        }
+
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.98f;
+	}
+}
